163b0d2e19ef25b8a4861b693a0c3574fa68b573
Kikoszzz <kikofcosta02@hotmail.com>
2026-01-02 22:21:08 +0000

    object_picking

--- VTK/Lesson_03/main.py
+++ VTK/Lesson_03/main.py
@@ -2,17 +2,40 @@
 Lesson 3 - Callbacks, Glyphing and Picking
 """
 
-from vtkmodules.vtkFiltersSources import vtkConeSource, vtkSphereSource
-from vtkmodules.vtkFiltersCore import vtkGlyph3D
-from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkPolyDataMapper,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer
-)
+from vtkmodules.vtkFiltersSources import *
+from vtkmodules.vtkFiltersCore import *
+from vtkmodules.vtkRenderingCore import *
+from vtkmodules.vtkCommonCore import *
+from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 import vtkmodules.vtkInteractionStyle
 import vtkmodules.vtkRenderingOpenGL2
+
+
+class callBack:
+    def __init__(self, picker, sphere_actor, render_window, text_mapper=None, text_actor=None):
+        self.picker = picker
+        self.sphere_actor = sphere_actor
+        self.render_window = render_window
+        self.text_mapper = text_mapper
+        self.text_actor = text_actor
+    
+    def __call__(self, caller, event):
+        pick_pos = self.picker.GetPickPosition()
+        print(f"Picked point coordinates: ({pick_pos[0]:.3f}, {pick_pos[1]:.3f}, {pick_pos[2]:.3f})")
+        
+        self.sphere_actor.SetPosition(pick_pos)
+        self.sphere_actor.VisibilityOn()
+        
+        if self.text_mapper and self.text_actor:
+            selection_point = self.picker.GetSelectionPoint()
+            
+            coord_text = f"3D: ({pick_pos[0]:.2f}, {pick_pos[1]:.2f}, {pick_pos[2]:.2f})\n2D: ({selection_point[0]:.0f}, {selection_point[1]:.0f})"
+            self.text_mapper.SetInput(coord_text)
+            
+            self.text_actor.SetPosition(selection_point[0] + 10, selection_point[1] + 10)
+            self.text_actor.VisibilityOn()
+        
+        self.render_window.Render()
 
 
 def glyphing():
@@ -23,6 +46,14 @@ def glyphing():
     sphereSource.SetThetaResolution(50)
     sphereSource.SetPhiResolution(50)
     
+    # Sphere actor - solid gray body
+    sphereMapper = vtkPolyDataMapper()
+    sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
+    
+    sphereActor = vtkActor()
+    sphereActor.SetMapper(sphereMapper)
+    
+    # Glyph actor - cones at each vertex
     glyph = vtkGlyph3D()
     glyph.SetSourceConnection(coneSource.GetOutputPort())
     glyph.SetInputConnection(sphereSource.GetOutputPort())
@@ -36,8 +67,8 @@ def glyphing():
     glyphActor.SetMapper(glyphMapper)
     
     renderer = vtkRenderer()
+    renderer.AddActor(sphereActor)
     renderer.AddActor(glyphActor)
-    renderer.SetBackground(0.1, 0.2, 0.4)
     
     renderWindow = vtkRenderWindow()
     renderWindow.AddRenderer(renderer)
@@ -47,9 +78,92 @@ def glyphing():
     interactor = vtkRenderWindowInteractor()
     interactor.SetRenderWindow(renderWindow)
     
+    # Configure o interactor style com o renderer
+    style = vtkInteractorStyleTrackballCamera()
+    style.SetCurrentRenderer(renderer)
+    interactor.SetInteractorStyle(style)
+    
+    renderWindow.Render()
+    interactor.Start()
+
+
+def object_picking():
+    coneSource = vtkConeSource()
+    coneSource.SetResolution(16)
+    
+    sphereSource = vtkSphereSource()
+    sphereSource.SetThetaResolution(8)
+    sphereSource.SetPhiResolution(8)
+    
+    sphereMapper = vtkPolyDataMapper()
+    sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
+    
+    sphereActor = vtkActor()
+    sphereActor.SetMapper(sphereMapper)
+    
+    glyph = vtkGlyph3D()
+    glyph.SetSourceConnection(coneSource.GetOutputPort())
+    glyph.SetInputConnection(sphereSource.GetOutputPort())
+    glyph.SetScaleFactor(0.2)
+    glyph.SetVectorModeToUseNormal()
+    
+    glyphMapper = vtkPolyDataMapper()
+    glyphMapper.SetInputConnection(glyph.GetOutputPort())
+    
+    glyphActor = vtkActor()
+    glyphActor.SetMapper(glyphMapper)
+    
+    pickerSphereSource = vtkSphereSource()
+    pickerSphereSource.SetRadius(0.05)
+    
+    pickerSphereMapper = vtkPolyDataMapper()
+    pickerSphereMapper.SetInputConnection(pickerSphereSource.GetOutputPort())
+    
+    pickerSphereActor = vtkActor()
+    pickerSphereActor.SetMapper(pickerSphereMapper)
+    pickerSphereActor.GetProperty().SetColor(1, 0, 0)
+    pickerSphereActor.VisibilityOff()
+    
+    textMapper = vtkTextMapper()
+    textProperty = textMapper.GetTextProperty()
+    textProperty.SetFontFamilyToCourier()
+    textProperty.SetFontSize(12)
+    textProperty.BoldOn()
+    textProperty.SetJustificationToCentered()
+    textProperty.SetColor(1, 1, 1)
+    
+    textActor = vtkActor2D()
+    textActor.SetMapper(textMapper)
+    textActor.VisibilityOff()
+    
+    renderer = vtkRenderer()
+    renderer.AddActor(sphereActor)
+    renderer.AddActor(glyphActor)
+    renderer.AddActor(pickerSphereActor)
+    renderer.AddActor(textActor)
+    
+    renderWindow = vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(800, 600)
+    renderWindow.SetWindowName("Lesson 3 - Object Picking")
+    
+    interactor = vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderWindow)
+    
+    style = vtkInteractorStyleTrackballCamera()
+    style.SetCurrentRenderer(renderer)
+    interactor.SetInteractorStyle(style)
+    
+    picker = vtkPointPicker()
+    callback = callBack(picker, pickerSphereActor, renderWindow, textMapper, textActor)
+    picker.AddObserver(vtkCommand.EndPickEvent, callback)
+    interactor.SetPicker(picker)
+    
     renderWindow.Render()
     interactor.Start()
 
 
 if __name__ == "__main__":
-    glyphing()
+    #glyphing()
+    #object_picking()
+    pass
