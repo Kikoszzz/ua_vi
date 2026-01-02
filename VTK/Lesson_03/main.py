755b3b43cce3f46f23fdf4ae72fe4e0a86b92417
Kikoszzz <kikofcosta02@hotmail.com>
2026-01-02 23:31:39 +0000

    .

--- VTK/Lesson_03/main.py
+++ VTK/Lesson_03/main.py
@@ -6,6 +6,7 @@ from vtkmodules.vtkFiltersSources import *
 from vtkmodules.vtkFiltersCore import *
 from vtkmodules.vtkRenderingCore import *
 from vtkmodules.vtkCommonCore import *
+from vtkmodules.vtkCommonDataModel import *
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 import vtkmodules.vtkInteractionStyle
 import vtkmodules.vtkRenderingOpenGL2
@@ -162,8 +163,176 @@ def object_picking():
     renderWindow.Render()
     interactor.Start()
 
+def unstructured_grid():
+    coords = [[0, 0, 0], [1, 0, 0], [0.5, 1, 0], [0.5, 0.5, 1]]
+    
+    ugrid = vtkUnstructuredGrid()
+    points = vtkPoints()
+    
+    for i in range(len(coords)):
+        points.InsertPoint(i, coords[i])
+    
+    for i in range(len(coords)):
+        ugrid.InsertNextCell(VTK_VERTEX, 1, [i])
+    
+    ugrid.SetPoints(points)
+    
+    ugridMapper = vtkDataSetMapper()
+    ugridMapper.SetInputData(ugrid)
+    
+    ugridActor = vtkActor()
+    ugridActor.SetMapper(ugridMapper)
+    ugridActor.GetProperty().SetColor(1, 0, 0)
+    ugridActor.GetProperty().SetPointSize(5)
+    
+    renderer = vtkRenderer()
+    renderer.AddActor(ugridActor)
+    
+    renderWindow = vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(800, 600)
+    renderWindow.SetWindowName("Lesson 3 - Unstructured Grid")
+    
+    interactor = vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderWindow)
+    
+    renderWindow.Render()
+    interactor.Start()
+
+
+def scalar_vector_association():
+    coords = [[0, 0, 0], [1, 0, 0], [0.5, 1, 0], [0.5, 0.5, 1]]
+    vectors = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1]]
+    scalars = [0.1, 0.3, 0.5, 0.8]
+    
+    ugrid = vtkUnstructuredGrid()
+    points = vtkPoints()
+    
+    for i in range(len(coords)):
+        points.InsertPoint(i, coords[i])
+    
+    for i in range(len(coords)):
+        ugrid.InsertNextCell(VTK_VERTEX, 1, [i])
+    
+    ugrid.SetPoints(points)
+    
+    vectorArray = vtkFloatArray()
+    vectorArray.SetNumberOfComponents(3)
+    vectorArray.SetName("Vectors")
+    
+    for vec in vectors:
+        vectorArray.InsertNextTuple3(vec[0], vec[1], vec[2])
+    
+    ugrid.GetPointData().SetVectors(vectorArray)
+    
+    scalarArray = vtkFloatArray()
+    scalarArray.SetNumberOfComponents(1)
+    scalarArray.SetName("Scalars")
+    
+    for scalar in scalars:
+        scalarArray.InsertNextValue(scalar)
+    
+    ugrid.GetPointData().SetScalars(scalarArray)
+    
+    coneSource = vtkConeSource()
+    coneSource.SetResolution(16)
+    
+    glyph = vtkGlyph3D()
+    glyph.SetSourceConnection(coneSource.GetOutputPort())
+    glyph.SetInputData(ugrid)
+    glyph.SetVectorModeToUseVector()
+    glyph.SetScaleModeToScaleByScalar()
+    glyph.SetScaleFactor(0.5)
+    glyph.OrientOn()
+    #glyph.OrientOff()
+    #glyph.SetColorModeToColorByScalar()
+    glyph.SetColorModeToColorByScalar()
+    
+    glyphMapper = vtkPolyDataMapper()
+    glyphMapper.SetInputConnection(glyph.GetOutputPort())
+    glyphMapper.SetScalarRange(0.1, 0.8)
+    
+    glyphActor = vtkActor()
+    glyphActor.SetMapper(glyphMapper)
+    
+    renderer = vtkRenderer()
+    renderer.AddActor(glyphActor)
+    
+    renderWindow = vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(800, 600)
+    renderWindow.SetWindowName("Lesson 3 - Scalar and Vector Association")
+    
+    interactor = vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderWindow)
+    
+    renderWindow.Render()
+    interactor.Start()
+
+
+def hedgehog():
+    coords = [[0, 0, 0], [1, 0, 0], [0.5, 1, 0], [0.5, 0.5, 1]]
+    vectors = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1]]
+    scalars = [0.1, 0.3, 0.5, 0.8]
+
+    ugrid = vtkUnstructuredGrid()
+    points = vtkPoints()
+
+    for i in range(len(coords)):
+        points.InsertPoint(i, coords[i])
+
+    for i in range(len(coords)):
+        ugrid.InsertNextCell(VTK_VERTEX, 1, [i])
+
+    ugrid.SetPoints(points)
+
+    vectorArray = vtkFloatArray()
+    vectorArray.SetNumberOfComponents(3)
+    vectorArray.SetName("Vectors")
+
+    for vec in vectors:
+        vectorArray.InsertNextTuple3(vec[0], vec[1], vec[2])
+
+    ugrid.GetPointData().SetVectors(vectorArray)
+
+    scalarArray = vtkFloatArray()
+    scalarArray.SetNumberOfComponents(1)
+    scalarArray.SetName("Scalars")
+
+    for scalar in scalars:
+        scalarArray.InsertNextValue(scalar)
+
+    ugrid.GetPointData().SetScalars(scalarArray)
+
+    hedgehog = vtkHedgeHog()
+    hedgehog.SetInputData(ugrid)
+    hedgehog.SetScaleFactor(0.4)
+
+    hedgeMapper = vtkPolyDataMapper()
+    hedgeMapper.SetInputConnection(hedgehog.GetOutputPort())
+    hedgeMapper.SetScalarRange(0.1, 0.8)
+
+    hedgeActor = vtkActor()
+    hedgeActor.SetMapper(hedgeMapper)
+
+    renderer = vtkRenderer()
+    renderer.AddActor(hedgeActor)
+
+    renderWindow = vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(800, 600)
+    renderWindow.SetWindowName("Lesson 3 - HedgeHog")
+
+    interactor = vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderWindow)
+
+    renderWindow.Render()
+    interactor.Start()
+
 
 if __name__ == "__main__":
     #glyphing()
     #object_picking()
-    pass
+    #unstructured_grid()
+    #scalar_vector_association()
+    hedgehog()
